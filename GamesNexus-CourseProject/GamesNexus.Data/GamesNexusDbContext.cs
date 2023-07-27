a4c890b23abb51da87f7c0a80eb16f054cbd107d
Mihail Stanchev <mihail.stanchev97@gmail.com>
2023-07-27 17:56:27 +0300

    Added To Do

--- GamesNexus-CourseProject/GamesNexus.Data/GamesNexusDbContext.cs
+++ GamesNexus-CourseProject/GamesNexus.Data/GamesNexusDbContext.cs
@@ -16,6 +16,16 @@ namespace GamesNexus.Data
         //TO DO Configure OnModelCreating
         //TO DO Make configuration for every entity
 
+//        Game:
+//-- Genre Many-to-Many
+//-- Category Many-to-Many
+//-- Review One-To-Many
+//-- Images One-to-many
+//-- Videos One-to-Many
+
+//Publisher:
+//-- Game One-to-Many
+
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
