67646ec89944728a1a92e46fab6571cdc6aeb5dc
Mihail Stanchev <mihail.stanchev97@gmail.com>
2023-08-09 20:32:23 +0300

    Implement custom Login

--- GamesNexus-CourseProject/GamesNexus.Web/Controllers/UserController.cs
+++ GamesNexus-CourseProject/GamesNexus.Web/Controllers/UserController.cs
@@ -1,9 +1,12 @@
 ﻿using GamesNexus.Data.Models;
 using GamesNexus.Web.ViewModels.User;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
+
+using static GamesNexus.Common.NotificationMessagesConstants;
 
 namespace GamesNexus.Web.Controllers
 {
@@ -13,7 +16,7 @@ namespace GamesNexus.Web.Controllers
         private readonly UserManager<ApplicationUser> userManager;
         private readonly IUserStore<ApplicationUser> userStore;
 
-        public UserController (SignInManager<ApplicationUser> _signInManager,
+        public UserController(SignInManager<ApplicationUser> _signInManager,
                                UserManager<ApplicationUser> _userManager,
                                IUserStore<ApplicationUser> _userStore)
         {
@@ -40,8 +43,8 @@ namespace GamesNexus.Web.Controllers
 
             ApplicationUser applicationUser = new ApplicationUser()
             {
-                 //UserName = model.UserName,
-                 //Email = model.Email,
+                //UserName = model.UserName,
+                //Email = model.Email,
             };
 
             await this.userManager.SetEmailAsync(applicationUser, model.Email);
@@ -65,5 +68,43 @@ namespace GamesNexus.Web.Controllers
 
 
         }
+
+        [AllowAnonymous]
+        [HttpGet]
+        public async Task<IActionResult> Login(string? returnUrl = null)
+        {
+            await HttpContext.SignOutAsync(IdentityConstants.ExternalScheme);
+
+            LoginFormModel model = new LoginFormModel()
+            {
+                ReturnUrl = returnUrl,
+            };
+
+            return this.View(model);
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginFormModel model)
+        {
+
+            if (!ModelState.IsValid)
+            {
+                return this.View(model);
+            }
+
+            var result =
+                await this.signInManager.PasswordSignInAsync(model.Username, model.Password, model.RememberMe, false);
+
+            if (!result.Succeeded)
+            {
+                this.TempData[ErrorMessage] = "There was an error while logging you in! Please try again later or contact an administrator.";
+
+                return this.View(model);
+            }
+
+            return this.Redirect(model.ReturnUrl ?? "/Home/Index");
+        }
+
     }
 }
--- GamesNexus-CourseProject/GamesNexus.Web/Program.cs
+++ GamesNexus-CourseProject/GamesNexus.Web/Program.cs
@@ -34,7 +34,11 @@ namespace GamesNexus.Web
                 .AddEntityFrameworkStores<GamesNexusDbContext>();
 
             builder.Services.AddApplicationServices();
-          
+
+            builder.Services.ConfigureApplicationCookie(cfg =>
+            {
+                cfg.LoginPath = "/User/Login";
+            });
 
             builder.Services.AddControllersWithViews()
                 .AddMvcOptions(options =>
