786e304453befc28260afc4b82ee2aa5bb287011
Mihail Stanchev <mihail.stanchev97@gmail.com>
2023-08-12 00:21:35 +0300

    Reset

--- GamesNexus-CourseProject/GamesNexus.Web.ViewModels/Game/GameAddFormModel.cs
+++ GamesNexus-CourseProject/GamesNexus.Web.ViewModels/Game/GameAddFormModel.cs
@@ -8,17 +8,5 @@ namespace GamesNexus.Web.ViewModels.Game
 {
     public class GameAddFormModel
     {
-        public long Id { get; set; }
-
-
-        public string Title { get; set; } = null!;
-
-
-        public string Description { get; set; } = null!;
-
-
-        public decimal Price { get; set; }
-
-        public string Developer { get; set; } = null!;
     }
 }
